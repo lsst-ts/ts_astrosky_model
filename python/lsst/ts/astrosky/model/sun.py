7f8f917ac26c2e22ade6225152f0ddfa53cc5525
Tiago Ribeiro <tiago.astro@gmail.com>
2021-06-22 11:27:26 -0700

    Remove unused `__init__` files. Add license header to all files.

--- python/lsst/ts/astrosky/model/sun.py
+++ python/lsst/ts/astrosky/model/sun.py
@@ -1,9 +1,29 @@
-# -*- coding: iso-8859-1 -*-
+# This file is part of ts_astrosky_model.
+#
+# Developed for the Vera Rubin Observatory Telescope and Site Systems.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+
 from __future__ import division
 from datetime import datetime
 import math
 
 __all__ = ["Sun"]
+
 
 class Sun(object):
     """
